5523726416c0502b05baee0defcea61dc5688588
Mathéo Lopes <matheo.lps91@gmail.com>
2024-02-04 15:53:43 +0100

    Github add

--- FrontEnd/src/Component/Footer.jsx
+++ FrontEnd/src/Component/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
                 <div className='socialContainer'>
                     <a href='https://www.linkedin.com/feed/' className='fa'><i class="fa-brands fa-linkedin-in"></i></a>
                     <a href='https://twitter.com/matholopes5' className='fa'><i class="fa-brands fa-x-twitter"></i></a>
-                    <a href='https://www.linkedin.com/feed/' className='fa'><i class="fa-brands fa-instagram"></i></a>
+                    <a href='https://github.com/MTLP2' className='fa'><i class="fa-brands fa-github"></i></a>
 
                 </div>
     </div>
